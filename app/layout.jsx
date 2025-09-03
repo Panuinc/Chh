11d17c19cfb37cad834aa355eae0b31bae2b2adb
Panuinc <panuwat.ja.forwork@gmail.com>
2025-09-03 15:09:43 +0700

    update

---
+++ app/layout.jsx
@@ -0,0 +1,45 @@
+import { Geist, Geist_Mono } from "next/font/google";
+import { Providers } from "./providers";
+import { ThemeProvider } from "next-themes";
+import "@/style/globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata = {
+  title: "EverGreen",
+  description: "EverGreen Development",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <head>
+        <link rel="icon" href="/logoCompany/logoCompany_1.png" />
+      </head>
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        <Providers>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="light"
+            enableSystem={true}
+            disableTransitionOnChange
+          >
+            <div className="flex items-center justify-center w-full h-screen gap-2">
+              {children}
+            </div>
+          </ThemeProvider>
+        </Providers>
+      </body>
+    </html>
+  );
+}

63785938c2069ea1fa74efa7ce4a859484bd4980
Panuinc <panuwat.ja.forwork@gmail.com>
2025-09-04 10:51:56 +0700

    update

--- app/(pages)/layout.jsx
+++ app/(pages)/layout.jsx
@@ -11,12 +11,12 @@ import { useState, useEffect } from "react";
 
 function MainMenu({ icons, text, collapsed }) {
   return (
-    <div className="flex flex-row items-center justify-center w-full p-1 gap-2 rounded-md border-1 border-dark">
-      <div className="flex items-center justify-center h-full p-1 gap-2 border-1 border-dark">
+    <div className="flex flex-row items-center justify-center w-full p-1 gap-1 border-1 border-dark">
+      <div className="flex items-center justify-center w-fit h-full p-1 gap-1 border-1 border-dark">
         {icons}
       </div>
       {!collapsed && (
-        <div className="flex items-center justify-start w-full h-full p-1 gap-2 border-1 border-dark">
+        <div className="flex items-center justify-center w-full h-full p-1 gap-1 border-1 border-dark">
           {text}
         </div>
       )}
@@ -35,12 +35,23 @@ export default function PagesLayout({ children }) {
     { icon: <User />, text: "Human Resource" },
     { icon: <Computer />, text: "Technology" },
     { icon: <Settings />, text: "Setting" },
+    { icon: <Settings />, text: "Setting" },
+    { icon: <Settings />, text: "Setting" },
+    { icon: <Settings />, text: "Setting" },
+    { icon: <Settings />, text: "Setting" },
+    { icon: <Settings />, text: "Setting" },
+    { icon: <Settings />, text: "Setting" },
+    { icon: <Settings />, text: "Setting" },
+    { icon: <Settings />, text: "Setting" },
+    { icon: <Settings />, text: "Setting" },
+    { icon: <Settings />, text: "Setting" },
+    { icon: <Settings />, text: "Setting" },
   ];
 
   return (
-    <div className="flex flex-col items-center justify-center w-full h-full">
-      <div className="flex flex-row items-center justify-center w-full h-fit border-b-1 border-secondary">
-        <div className="flex items-center justify-center w-[5%] h-full p-1 gap-2 border-1 border-dark">
+    <div className="flex flex-col items-center justify-center w-full h-full p-1 gap-1 border-1 border-dark">
+      <div className="flex flex-row items-center justify-center w-full h-fit p-1 gap-1 border-1 border-dark">
+        <div className="flex items-center justify-center w-[5%] h-full p-1 gap-1 border-1 border-dark">
           <Image
             src="/logoCompany/logoCompany_1.png"
             alt="logoCompany"
@@ -49,34 +60,34 @@ export default function PagesLayout({ children }) {
             priority
           />
         </div>
-        <div className="flex items-center justify-start w-[10%] h-full p-1 gap-2 border-1 border-dark">
+        <div className="flex items-center justify-center w-[10%] h-full p-1 gap-1 border-1 border-dark">
           Panuinc's Org
         </div>
-        <div className="flex items-center justify-center w-[85%] h-full p-1 gap-2 border-1 border-dark">
+        <div className="flex items-center justify-center w-[85%] h-full p-1 gap-1 border-1 border-dark">
           1
         </div>
       </div>
 
-      <div className="flex flex-row items-center justify-start w-full h-full border-t-1 border-secondary overflow-auto">
+      <div className="flex flex-row items-center justify-center w-full h-full p-1 gap-1 border-1 border-dark overflow-auto">
         <div
-          className={`flex flex-col items-center justify-start ${
-            collapsed ? "w-[5%]" : "w-[15%]"
-          } h-full gap-2 border-r-1 border-secondary overflow-auto transition-all duration-300 ease-in-out`}
+          className={`flex flex-col items-center justify-center ${
+            collapsed ? "w-fit" : "w-[15%]"
+          } h-full p-1 gap-1 border-1 border-dark transition-all duration-300 ease-in-out`}
         >
-          <div className="flex flex-col items-center justify-start w-full p-1 gap-2">
-            <div className="flex flex-row items-center justify-center w-full p-1 gap-2 rounded-md border-1 border-dark">
-              <div className="flex items-center justify-center h-full p-1 gap-2 border-1 border-dark">
+          <div className="flex flex-col items-center justify-center w-full p-1 gap-1 border-1 border-dark">
+            <div className="flex flex-row items-center justify-center w-full p-1 gap-1 border-1 border-dark">
+              <div className="flex items-center justify-center w-fit h-full p-1 gap-1 border-1 border-dark">
                 <LayoutDashboard />
               </div>
               {!collapsed && (
-                <div className="flex items-center justify-start w-full h-full p-1 gap-2 border-1 border-dark">
+                <div className="flex items-center justify-center w-full h-full p-1 gap-1 border-1 border-dark">
                   Overview
                 </div>
               )}
             </div>
           </div>
 
-          <div className="flex flex-col items-center justify-start w-full h-full p-1 gap-2 overflow-auto">
+          <div className="flex flex-col items-center justify-start w-full h-full p-1 gap-1 border-1 border-dark overflow-auto">
             {menus.map((menu, idx) => (
               <MainMenu
                 key={idx}
@@ -87,12 +98,12 @@ export default function PagesLayout({ children }) {
             ))}
           </div>
 
-          <div className="flex flex-col items-center justify-center w-full p-1 gap-2">
+          <div className="flex flex-col items-center justify-center w-full p-1 gap-1 border-1 border-dark">
             <button
               onClick={() => setCollapsed((prev) => !prev)}
-              className="flex flex-row items-center justify-start w-full p-1 gap-2 rounded-md border-1 border-dark"
+              className="flex flex-row items-center justify-end w-full p-1 gap-1 border-1 border-dark"
             >
-              <div className="flex items-center justify-center h-full p-1 gap-2 border-1 border-dark">
+              <div className="flex items-center justify-center w-fit h-full p-1 gap-1 border-1 border-dark">
                 <PanelLeftDashed />
               </div>
             </button>
@@ -101,10 +112,10 @@ export default function PagesLayout({ children }) {
 
         <div
           className={`flex flex-col items-center justify-center ${
-            collapsed ? "w-[95%]" : "w-[85%]"
-          } h-full gap-2 border-l-1 border-secondary overflow-auto transition-all duration-300 ease-in-out`}
+            collapsed ? "w-full" : "w-[85%]"
+          } h-full p-1 gap-1 border-1 border-dark transition-all duration-300 ease-in-out`}
         >
-          <div className="flex flex-col items-center justify-start w-full h-full p-1 gap-2 border-1 border-danger overflow-auto">
+          <div className="flex flex-col items-center justify-start w-full h-full p-1 gap-1 border-1 border-dark overflow-auto">
             {children}
           </div>
         </div>

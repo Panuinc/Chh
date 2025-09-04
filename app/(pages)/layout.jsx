4aa750040171798e1826c707dc5d742c6e32cdb1
Panuinc <panuwat.ja.forwork@gmail.com>
2025-09-04 10:02:48 +0700

    update

--- app/(pages)/layout.jsx
+++ app/(pages)/layout.jsx
@@ -1,4 +1,10 @@
-import { LayoutDashboard, PanelLeftDashed, Settings } from "lucide-react";
+import {
+  Computer,
+  LayoutDashboard,
+  PanelLeftDashed,
+  Settings,
+  User,
+} from "lucide-react";
 import Image from "next/image";
 
 function MainMenu({ icons, text }) {
@@ -40,13 +46,26 @@ export default function PagesLayout({ children }) {
         <div className="flex flex-row items-center justify-start w-full h-full border-t-1 border-secondary overflow-auto">
           <div className="flex flex-col items-center justify-start w-[15%] h-full gap-2 border-r-1 border-secondary overflow-auto">
             <div className="flex flex-col items-center justify-start w-full p-1 gap-2">
-              <MainMenu icons={<LayoutDashboard />} text="Overview" />
+              <div className="flex flex-row items-center justify-center w-full p-1 gap-2 rounded-md border-1 border-dark">
+                <div className="flex items-center justify-center h-full p-1 gap-2 border-1 border-dark">
+                  <LayoutDashboard />
+                </div>
+                <div className="flex items-center justify-start w-full h-full p-1 gap-2 border-1 border-dark">
+                  Overview
+                </div>
+              </div>
             </div>
             <div className="flex flex-col items-center justify-start w-full h-full p-1 gap-2 overflow-auto">
+              <MainMenu icons={<User />} text="Human Resource" />
+              <MainMenu icons={<Computer />} text="Technology" />
               <MainMenu icons={<Settings />} text="Setting" />
             </div>
-            <div className="flex flex-col items-center justify-start w-full p-1 gap-2">
-              <MainMenu icons={<PanelLeftDashed />} />
+            <div className="flex flex-col items-center justify-center w-full p-1 gap-2">
+              <div className="flex flex-row items-center justify-start w-full p-1 gap-2 rounded-md border-1 border-dark">
+                <div className="flex items-center justify-center h-full p-1 gap-2 border-1 border-dark">
+                  <PanelLeftDashed />
+                </div>
+              </div>
             </div>
           </div>
           <div className="flex flex-col items-center justify-center w-[85%] h-full gap-2 border-l-1 border-secondary overflow-auto">
